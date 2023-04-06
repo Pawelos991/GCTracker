4338d5c8038615372ec070932c66f8b3fcbc3cf2
Jakub Miętus <hidmir305@gmail.com>
2023-04-06 22:40:30 +0200

    Create ProductData class.

---
+++ GCTracker_Scrawler/Scrawler/Data/ProductData.cs
@@ -0,0 +1,10 @@
+namespace GCTracker_Scrawler.Scrawler.Data;
+
+public class ProductData
+{
+	//TODO: Fill class with variables describing the target data. 
+	public string Name { get; set; }
+	public string Price { get; set; }
+	public string ProducentCode { get; set; }
+	public string ImageAddress { get; set; }
+}
