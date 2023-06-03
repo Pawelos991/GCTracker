b246a666e790516ba8f5cd985df98d9f41262e08
Jakub Miętus <hidmir305@gmail.com>
2023-06-03 12:46:05 +0200

    Remove MOCK_DATA

--- GCTracker_Scrawler/Program.cs
+++ GCTracker_Scrawler/Program.cs
@@ -53,22 +53,3 @@ static void LogProductsData(List<Product> products)
 	}
 }
 
-public static class MOCK_DATA
-{
-    public static Product SingleProduct()
-    {
-		return new Product() { ImageAddress = "Addr", Name = "Mock", Price = 432.32m, ProducentCode = "PrCodeMock"} ;
-    }
-
-    public static List<Product> ManyProducts()
-    {
-		var listToRet = new List<Product>();
-		listToRet.Add(new Product() { ImageAddress = "Addr", Name = "Mock", Price = 43243m, ProducentCode = "PrCodeMock8888" });
-        listToRet.Add(new Product() { ImageAddress = "Addr123", Name = "Moc123k", Price = 4423m, ProducentCode = "PrCodeMock1111888" });
-        listToRet.Add(new Product() { ImageAddress = "Addr33", Name = "Mock4534", Price = 0.99m, ProducentCode = "PrCodeMock334888" });
-        listToRet.Add(new Product() { ImageAddress = "Addr232", Name = "Mock23123", Price = 43.355m, ProducentCode = "PrCodeMock2342888" });
-        listToRet.Add(new Product() { ImageAddress = "Addr123123", Name = "Mock343242", Price = 111.3m, ProducentCode = "PrCodeMock2228888" });
-        return listToRet;
-    }
-}
-
